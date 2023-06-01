b615f1d3ce71e27e6a0d64ffc3d5bc98a46e0e99
david5871 <davidbarath08@gmail.com>
2023-06-01 19:17:52 +0200

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontendComponent } from './frontend/frontend.component';
+import { BackendComponent } from './backend/backend.component';
+import { JavacmdComponent } from './javacmd/javacmd.component';
+import { JavaguiComponent } from './javagui/javagui.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'frontend', component: FrontendComponent },
+  { path: 'backend', component: BackendComponent },
+  { path: 'javacli', component: JavacmdComponent },
+  { path: 'javagui', component: JavaguiComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
